7c3429c93ddfc4e4108cf98feab35f2bd985e71f
HoqiheChen <851774342@qq.com>
2019-01-20 23:51:35 +0800

    寒假实训2019.01.19作业（完成Exercise2、3，剩余Exercise1）

--- chapter1/homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises2.java
+++ chapter1/homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises2.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 
 /**
  * 作业2：一个抖音笔试题：统计页面所有view的个数
@@ -15,11 +20,32 @@ public class Exercises2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        View view;
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_relativelayout);
+        view = findViewById(R.id.rootgc);
+        int result = this.getAllChildViewCount(view);
+        final String res = Integer.toString(result);
+       final TextView textView = findViewById(R.id.tv_center);
+       final Button button = findViewById(R.id.search);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(res);
+            }
+        });
+
     }
 
     public int getAllChildViewCount(View view) {
-        //todo 补全你的代码
-        return 0;
+
+        ViewGroup viewGroup =(ViewGroup)view;
+
+        int count = viewGroup.getChildCount();
+
+        return count;
     }
 }
