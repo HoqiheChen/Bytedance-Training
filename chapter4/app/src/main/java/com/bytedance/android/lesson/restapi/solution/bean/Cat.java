b5db85381f55a123912179319a3af25095e93d80
HoqiheChen <851774342@qq.com>
2019-01-22 16:03:01 +0800

    寒假实训2019.01.22作业（完成作业一）

---
+++ chapter4/app/src/main/java/com/bytedance/android/lesson/restapi/solution/bean/Cat.java
@@ -0,0 +1,50 @@
+package com.bytedance.android.lesson.restapi.solution.bean;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Arrays;
+
+/**
+ * @author Xavier.S
+ * @date 2019.01.17 18:08
+ */
+public class Cat {
+
+    // TODO-C1 (1) Implement your Cat Bean here according to the response json
+    @SerializedName("breeds") private String []breeds;
+    @SerializedName("id")private String id;
+    @SerializedName("url") private String url;
+
+    public String[] getBreeds() {
+        return breeds;
+    }
+
+    public void setBreeds(String[] breeds) {
+        this.breeds = breeds;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "breeds=" + Arrays.toString(breeds) +
+                ", id='" + id + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
