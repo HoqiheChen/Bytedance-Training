bb17d9478a0231be1878185b775c0e91a5e05edb
HoqiheChen <851774342@qq.com>
2019-01-26 15:50:24 +0800

    寒假实训2019.01.26作业（跑通先来一波儿）

---
+++ chapter8/JNIDemo/app/src/main/java/bytedance/com/jnidemo/IndexActivity.java
@@ -0,0 +1,30 @@
+package bytedance.com.jnidemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class IndexActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+
+        findViewById(R.id.common_preview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(IndexActivity.this, MainActivity.class);
+                startActivity(it);
+            }
+        });
+
+//        findViewById(R.id.gl_recorder).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent it = new Intent(IndexActivity.this, GLMainActivity.class);
+//                startActivity(it);
+//            }
+//        });
+    }
+}
